a33caea352ea709c811727fcea0d8ec7adbc696a
Taehoon Kang <taehoon@uber.com>
2016-08-23 09:45:08 -0700

    Got rid of sync-request dependency.

--- src/shared/actions/get-user.js
+++ src/shared/actions/get-user.js
@@ -1,26 +1,29 @@
-import request from 'sync-request';
+import request from 'request';
 
 export default (accessToken) => (dispatch, getState) => {
   const url = `${location.protocol}//${location.host}/user`;
-  const response = request('GET', url, {
-    qs: {accessToken}
-  });
+  const response = request.get({
+    url,
+    qs: {accessToken},
+    json: true,
+  }, function (error, response, user) {
+    if (response.statusCode === 200) {
+      const {count, name} = user;
+      const payload = {accessToken, count, name};
 
-  if (response.statusCode === 200) {
-    const user = JSON.parse(response.body);
-    const {count, name} = user;
-    const payload = {accessToken, count, name};
+      dispatch({
+        type: 'GET_USER_SUCCESS',
+        payload
+      });
+      return;
+    }
+    const payload = Object.assign({
+      accessToken
+    }, getState());
 
     dispatch({
-      type: 'GET_USER_SUCCESS',
+      type: 'GET_USER_FAILURE',
       payload
     });
-    return;
-  }
-  dispatch({
-    type: 'GET_USER_FAILURE',
-    payload: Object.assign({
-      accessToken
-    }, getState())
   });
 };
