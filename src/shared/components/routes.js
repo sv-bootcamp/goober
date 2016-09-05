55772229a91b128f2ce26862c3954c14894207dd
Youngchan Je <ochanje210@gmail.com>
2016-09-05 16:16:37 +0900

    now header comes with map component

--- src/shared/components/routes.js
+++ src/shared/components/routes.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import AppContainer from '../containers/app-container';
 import IndexContainer from '../containers/index-container';
+import HeaderContainer from '../containers/header-container';
 
 import MapLayout from '../components/MapLayout';
-import header from '../components/header';
 
 export default (
   <Route path="/" component={AppContainer}>
     <IndexRoute component={IndexContainer}/>
-    <Route path="map" component={MapLayout}/>
-    <Route path="header" component={header}/>
+    <Route path="map" component={HeaderContainer}>
+      <IndexRoute component={MapLayout}/>
+    </Route>
   </Route>
 );
