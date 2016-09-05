62510dce9523adbb0a489dea1ee9fbd4f4d78612
Youngchan Je <ochanje210@gmail.com>
2016-09-05 14:06:29 +0900

    changed class name in header.js

--- src/shared/components/header.js
+++ src/shared/components/header.js
@@ -1,3 +1,23 @@
 /**
- * Created by wpdud on 2016-09-05.
+ * Created by Youngchan Je on 2016-09-05.
  */
+import React, {Component} from 'react';
+
+let divStyle = {
+  color: 'yellow',
+  backgroundColor: 'blue',
+  height: '200px',
+  width: '100%'
+};
+
+class Header extends Component {
+  render() {
+    return (
+      <div style={divStyle}>
+        WHAT A GREAT HEADER
+      </div>
+    );
+  }
+}
+
+export default Header;
