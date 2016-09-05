6776e056843dff89c0499ce3633c454e19b23e5e
Youngchan Je <ochanje210@gmail.com>
2016-09-05 14:04:44 +0900

    initial header without container

---
+++ src/shared/components/header.js
@@ -0,0 +1,3 @@
+/**
+ * Created by wpdud on 2016-09-05.
+ */
--- src/shared/components/routes.js
+++ src/shared/components/routes.js
@@ -4,10 +4,12 @@ import AppContainer from '../containers/app-container';
 import IndexContainer from '../containers/index-container';
 
 import MapLayout from '../components/MapLayout';
+import header from '../components/header';
 
 export default (
   <Route path="/" component={AppContainer}>
     <IndexRoute component={IndexContainer}/>
     <Route path="map" component={MapLayout}/>
+    <Route path="header" component={header}/>
   </Route>
 );
