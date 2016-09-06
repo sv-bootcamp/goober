ad81da6d4df48dcd15b95657340ccfacbe6c1716
ovekyc <ovekyc@gmail.com>
2016-09-06 16:25:34 +0900

    delete an item and edit routes

--- src/server/items/controllers.js
+++ src/server/items/controllers.js
@@ -1,7 +1,7 @@
 import db from '../database';
 
 export default {
-  getAll: (res, cb) => {
+  getAll: (req, res, cb) => {
     let result = {};
     db.createReadStream({
       start: 'item1',
@@ -10,9 +10,25 @@ export default {
       result[data.key] = data.value;
     }).on('close', () => {
       res.send(result);
-      if (cb) {
-        cb();
+      cb();
+    });
+  },
+
+  remove: (req, res, cb) => {
+    const key = 'item' + req.params.id;
+    db.del(key, (err) => {
+      if (err) {
+        res.status(400);
+        res.send({
+          message: err
+        });
+      } else {
+        res.status(200);
+        res.send({
+          message: 'success'
+        });
       }
+      cb();
     });
   }
 };
