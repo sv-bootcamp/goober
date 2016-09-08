896e7856c0b18f6d011128c4fa2099e7124c6b02
Patrick Shim <nasuk0610@gmail.com>
2016-09-08 01:47:42 +0000

    add items get api

--- src/server/items/controllers.js
+++ src/server/items/controllers.js
@@ -2,18 +2,56 @@ import db from '../database';
 
 export default {
   getAll: (req, res, cb) => {
-    const result = {};
+    const result = {
+      items: []
+    };
     db.createReadStream({
       start: 'item1',
       end: 'item' + '\xFF'
     }).on('data', (data) => {
-      result[data.key] = data.value;
-    }).on('close', () => {
-      res.send(result);
-      cb();
+      result.items.push(data.value);
+    }).on('error', (err) => {
+      if(err.notFound) {
+        res.status(200);
+        res.send(result);
+        return cb(result);
+      }
+      res.status(500);
+      res.send({
+        error: "database error"
+      })
+      return cb(err);
+    })
+    .on('close', () => {
+      if(result.items.length !== 0 ) {
+        res.send(result);
+        cb();
+      }
     });
   },
-
+  getById: (req, res, cb) => {
+    const key = 'item' + req.params.id;
+    db.get(key, (err, value) => {
+      if(err){
+        if(err.notFound) {
+          res.status(400);
+          res.send({
+            error: "Item was not found."
+          });
+          return cb(err);
+        }
+        res.status(500);
+        res.send({
+          error: "database error"
+        })
+        return cb(err);
+      }
+      
+      res.status(200);
+      res.send(value);
+      return cb();
+    });
+  },
   remove: (req, res, cb) => {
     const key = 'item' + req.params.id;
     db.del(key, (err) => {
