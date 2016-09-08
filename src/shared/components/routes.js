98717b81fd3ee8d510f553acbc13d24a5a69e23b
Youngchan Je <ochanje210@gmail.com>
2016-09-08 13:49:29 +0900

    deleted header container

--- src/shared/components/routes.js
+++ src/shared/components/routes.js
@@ -2,14 +2,13 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import AppContainer from '../containers/app-container';
 import IndexContainer from '../containers/index-container';
-import HeaderContainer from '../containers/header-container';
 
 import MapLayout from '../components/MapLayout';
 
 export default (
   <Route path="/" component={AppContainer}>
     <IndexRoute component={IndexContainer}/>
-    <Route path="map" component={HeaderContainer}>
+    <Route path="map" component={AppContainer}>
       <IndexRoute component={MapLayout}/>
     </Route>
   </Route>
