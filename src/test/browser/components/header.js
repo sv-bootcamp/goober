2a103eb9bbfd807245585e4db558d581d58d5217
Youngchan Je <ochanje210@gmail.com>
2016-09-13 11:31:34 +0900

    added header test case and fixed some build warnings

--- src/test/browser/components/header.js
+++ src/test/browser/components/header.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import Code from 'code';
+import Lab from 'lab';
+import { shallow } from 'enzyme';
 import Header from '../../../shared/components/header';
 
-it('Check text message in the header', () => {
-  const checkText = TestUtils.renderIntoDocument(
-    <Header/>
-  );
+const lab = exports.lab = Lab.script();
+const suite = lab.suite;
+const test = lab.test;
+const expect = Code.expect;
 
-  const checkTextNode = ReactDOM.findDOMNode(checkText);
-
-  expect(checkTextNode.textContent).toEqual('WHAT A GREAT HEADER');
+suite('Header Component', () => {
+  test('check header text', (done) => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper.text()).to.equal('asdf');
+    done();
+  });
 });
-
-it('Check CSS style', () => {
-  const checkStyle = TestUtils.renderIntoDocument(
-    <Header/>
-  );
-
-  const checkStyleNode = ReactDOM.findDOMNode(checkStyle);
-
-  expect(checkStyleNode.style._values).toEqual(
-    {
-      'color': 'yellow',
-      'background-color': 'blue',
-      'height': '200px',
-      'width': '100%'
-    }
-  );
-});
-
