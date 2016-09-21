531a2ad4e199fe81f35f2f2744fdf9e705d1317b
Youngchan Kim <ovekyc@gmail.com>
2016-09-21 14:28:30 +0900

    add Error handle module (#41)
    * add Error handle module
    
    * edit remove an item anomaly

--- src/server/routes/api-routes.js
+++ src/server/routes/api-routes.js
@@ -5,4 +5,25 @@ const router = express.Router();
 
 router.use('/items', items);
 
+router.use('/*', (req, res) => {
+  if (res.statusCode === 200 || res.statusCode === 304) {
+    return;
+  }
+  res.status(404).send('API Request > 404 - Page Not Found');
+});
+
+router.use((errHandler, req, res, next) => {
+  res.status(errHandler.statusCode).send({
+    error: errHandler.message
+  });
+  next(errHandler.error);
+});
+
+/*
+ we need to fix error message
+ there is two case
+ 1. prod
+ 2. dev
+ */
+
 export default router;
