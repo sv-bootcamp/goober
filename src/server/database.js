557fdc8d1dc28942fff09e2e93ae43fb78cdfdf0
Youngchan Kim <ovekyc@gmail.com>
2016-09-29 11:42:42 +0900

    Fix lint warning

--- src/server/database.js
+++ src/server/database.js
@@ -5,23 +5,20 @@ const db = levelup(config.database, {valueEncoding: 'json'});
 export default db;
 
 export const clearDB = (cb) => {
-  console.log('clearDB() : in');
-  
-   const errorList = [];
-    db.createReadStream({
-      start: '0',
-      end: '\xFF'
-    }).on('data', (data) => {
-      db.del(data.key, (err) => {
-        if (err) {
-          errorList.push(err);
-        }
-      });
-    }).on('close', () => {
-      if (errorList.length > 0) {
-        return cb(errorList);
+  const errorList = [];
+  db.createReadStream({
+    start: '0',
+    end: '\xFF'
+  }).on('data', (data) => {
+    db.del(data.key, (err) => {
+      if (err) {
+        errorList.push(err);
       }
-      console.log('clearDB() : success');
-      return cb();
     });
-}
+  }).on('close', () => {
+    if (errorList.length > 0) {
+      return cb(errorList);
+    }
+    return cb();
+  });
+};
