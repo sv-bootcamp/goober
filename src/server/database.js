659858665607637fc0126b63a3808bc9394a66a8
Hewon Jeong <hewon.jng@gmail.com>
2016-09-29 02:25:12 +0000

    Add database clear method

--- src/server/database.js
+++ src/server/database.js
@@ -1,4 +1,27 @@
 import levelup from 'levelup';
 import config from 'config';
 
-export default levelup(config.database, {valueEncoding: 'json'});
+const db = levelup(config.database, {valueEncoding: 'json'});
+export default db;
+
+export const clearDB = (cb) => {
+  console.log('clearDB() : in');
+  
+   const errorList = [];
+    db.createReadStream({
+      start: '0',
+      end: '\xFF'
+    }).on('data', (data) => {
+      db.del(data.key, (err) => {
+        if (err) {
+          errorList.push(err);
+        }
+      });
+    }).on('close', () => {
+      if (errorList.length > 0) {
+        return cb(errorList);
+      }
+      console.log('clearDB() : success');
+      return cb();
+    });
+}
