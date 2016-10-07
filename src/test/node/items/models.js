dc3c3b09fac27eb113c44b317c29591d9bd371c8
Hewon Jeong <hewon.jng@gmail.com>
2016-10-07 17:53:46 +0900

    add timestamp class test (#55)
    *  add timestamp class test
    
    *  modified module test code
    
    *  changed some vars as const and removed magic number

--- src/test/node/items/models.js
+++ src/test/node/items/models.js
@@ -1,11 +1,13 @@
 import test from 'tape';
 import geohash from 'ngeohash';
-import {KeyMaker, ALIVE, DEFAULT_PRECISON, MAX_TIME} from '../../../server/items/models';
+import {KeyMaker, ALIVE, DEFAULT_PRECISON, MAX_TIME, Timestamp, GEOHASH_START_POS, GEOHASH_END_POS,
+        REF_GEOHASH_START_POS, STATUS_CODE_POS} from '../../../server/items/models';
 
 const mockData = {
   lat: 37.565398,
   lng: 126.9907941,
-  date: '2016-10-04T04:00:00.578Z',
+  dateString: '2016-10-04T04:00:00.578Z',
+  dateObject: new Date(),
   precision: DEFAULT_PRECISON
 };
 
@@ -21,15 +23,30 @@ test('make alive key stream', t => {
   const keyStream = keyMaker.getKeyStream();
   const itemKey = keyStream[0];
 
-  t.equal(itemKey.slice(5, 13), expected.hash, 'should be same geohash');
-  t.equal(itemKey.includes(uuid), true, 'should have same uuid');
+  t.equal(itemKey.slice(GEOHASH_START_POS, GEOHASH_END_POS + 1), expected.hash,
+  'should be same geohash');
+  t.equal(itemKey.includes(uuid), true,
+  'should have same uuid');
 
   for (let i = 1; i <= mockData.precision; i = i + 1) {
-    t.equal(keyStream[i].slice(5, 6), expected.type, 'should have same type');
-    t.equal(keyStream[i].slice(7, 7 + i), expected.hash.slice(0, i), 'should have same geohash');
+    t.equal(keyStream[i].charAt(STATUS_CODE_POS), expected.type, 'should have same type');
+    t.equal(keyStream[i].slice(REF_GEOHASH_START_POS, REF_GEOHASH_START_POS + i),
+    expected.hash.slice(0, i), 'should have same geohash');
     t.equal(keyStream[i].includes(expected.time), true, 'should have same time');
     t.equal(keyStream[i].includes(uuid), true, 'should have same uuid');
   }
 
+  t.end();
+});
+
+test('make timestamp using module', t => {
+  const expected = {
+    stringResult: MAX_TIME - Number(new Date(mockData.dateString)),
+    dateResult: MAX_TIME - Number(mockData.dateObject)
+  };
+  const stringTS = new Timestamp(mockData.dateString).getTimestamp();
+  t.equal(stringTS, expected.stringResult, 'should have same timeStamp(String use)');
+  const dateTS = new Timestamp(mockData.dateObject).getTimestamp();
+  t.equal(dateTS, expected.dateResult, 'should have same timeStamp(Date use)');
   t.end();
 });
