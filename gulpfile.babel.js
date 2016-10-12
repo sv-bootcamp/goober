7a504185d058797e347f11069a5fd5803200f221
Patrick Shim <nasuk0610@gmail.com>
2016-10-12 15:55:21 +0900

    Test uintest

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -93,8 +93,8 @@ gulp.task('run:test', ['build:test'], () => {
   }, (exitCode) => {
     if (exitCode !== 0) {
       console.error('Tests failed! - Test script exited with non-zero status code.');
-      return false;
     }
+    process.exitCode = exitCode;
     return true;
   });
   output.pipe(process.stdout);
