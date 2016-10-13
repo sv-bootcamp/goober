79345909e824fdb2076a2c07fc9ae763a25e8aec
Youngchan Kim <ovekyc@gmail.com>
2016-10-13 15:56:47 +0900

    Add image put function (#62)
    * Add image put function
    * Fill out putImage
    * Add test code
    
    * Add S3 connector functions
    
    * Add getImageUrls function
    * Add delImage function
    * Add test code
    * Add MockS3 class
    
    * Edit aws config json file name
    
    * Add ignore aws config
    
    * Edit configuration
    
    * Add todo
    * Edit configuration files
    
    * Add callback parameter comments

--- src/server/aws-s3.js
+++ src/server/aws-s3.js
@@ -1,56 +1,125 @@
 import AWS from 'aws-sdk';
 import config from 'config';
+import fs from 'fs';
 
 AWS.config.loadFromPath(config.awsConfig);
 
-export class S3Connector {
-  constructor() {
-    this.s3instance = new AWS.S3();
+class MockS3 {
+  // We can trust AWS-sdk
+  // This class is just for test
+  constructor(configuration) {
+    const json = fs.readFileSync(configuration, 'utf8');
+    const awsConfig = JSON.parse(json);
+    this.endpoint = {};
+    this.endpoint.protocol = 'https:';
+    this.endpoint.host = `s3.${awsConfig.region}.amazonaws.com`;
   }
 
-  getImage(opt = {}, cb = () => {}) {
-    // opt {
-    //   itemid: 'id',
-    //   imageid: 'id'
-    // }
-    console.log(opt); // will be removed
-    this.s3instance.listObjects({Bucket: config.awsImageBucket}, (err, data) => {
-      if (err) {
-        return cb(err);
-      }
-      return cb(null, data);
+  getSignedUrl() {
+    return 'some-url';
+  }
+
+  putObject(param = {}, cb = () => {}) {
+    if (!param.Bucket || !param.Key || !param.Body) {
+      cb(new Error('Error, Invalid parameter'));
+      return;
+    }
+    cb(null, {ETag: 'some-tag-code'});
+  }
+
+  deleteObject(params = {}, cb = () => {}) {
+    cb(null, params);
+  }
+}
+
+export class S3Connector {
+  constructor() {
+    if (process.env.NODE_ENV === 'test') {
+      // TODO: Real S3 instance test
+      // Make sure that
+      // You SHOULD test with real s3 instance when edit S3Connector
+      // You have to change value of awsConfig of 'config/test.json'
+      //   as 'aws.json' and fill 'aws.json' with proper values for real s3 instance test
+      // this code is just for No more request at CI time (aws pretty expensive)
+      this.s3instance = new MockS3(config.awsConfig);
+    } else {
+      this.s3instance = new AWS.S3();
+    }
+  }
+
+  getImageUrls(keys = [], cb = () => {}) {
+    // input parameters
+    // keys = ['key1', 'key2'];
+    //
+    // callback parameters
+    // err : err
+    // data : array of url
+    if (keys.length === 0) {
+      cb(null, []);
+      return;
+    }
+    const promises = [];
+    const urlList = [];
+    for (const key of keys) {
+      promises.push(new Promise((resolve, reject) => {
+        const params = {
+          Bucket: config.awsImageBucket,
+          Key: key,
+          Expires: 120
+        };
+        const url = this.s3instance.getSignedUrl('getObject', params);
+        if (!url) {
+          reject('No url');
+        }
+        // remove query string
+        urlList.push(url.split('?')[0]);
+        resolve();
+      }));
+    }
+    Promise.all(promises).then(() => {
+      cb(null, urlList);
+    }).catch((err) => {
+      cb(err);
     });
   }
 
-  putImage(opt = {}) {
-    console.log(opt); // will be removed
-  }
-
-  delImage(opt = {}) {
-    console.log(opt); // will be removed
-  }
-
-}
-/*
-class MockS3 {
-  constructor() {}
-
-  listObjects(opt={}, cb=this.next) {
-    if (!opt.Bucket) {
-      const err = new Error('No Bucket');
-      return cb(err);
+  putImage(opt = {}, cb = () => {}) {
+    // input parameters
+    // opt = {
+    //   key; 'data-key',
+    //   body: 'some string (image-encoded-base64)'
+    // }
+    //
+    // callback parameter
+    // err  : error when fail put image
+    // data : { ETag : 'etag string'}
+    if (!opt.key || !opt.body) {
+      cb(new Error('Wrong parameter'));
+      return;
     }
-    return {
-      IsTruncated: false,
-      Marker: opt.Marker ? opt.Marker : '',
-      Contents: [],
-      Name: opt.Bucket,
-      Prefix: '',
-      MaxKeys: 1000,
-      CommonPrefixes: []
+    const params = {
+      Bucket: config.awsImageBucket,
+      Key: opt.key,
+      Body: opt.body,
+      ACL: 'public-read'
     };
+    this.s3instance.putObject(params, cb);
   }
 
-  next(err=null, data={}) {}
+  delImage(opt = {}, cb = () => {}) {
+    // input parameters
+    // opt = {
+    //   key; 'data-key'
+    // }
+    if (!opt.key) {
+      cb(new Error('Wrong parameter'));
+      return;
+    }
+    const params = {
+      Bucket: config.awsImageBucket,
+      Key: opt.key
+    };
+    this.s3instance.deleteObject(params, cb);
+  }
+
 }
-*/
--- src/test/node/aws-s3.js
+++ src/test/node/aws-s3.js
@@ -22,3 +22,72 @@ test('test aws instance', t => {
     return t.end();
   });
 });
+test('test s3 put image', t => {
+  fs.readFile('src/test/node/test.png', (errRead, image) => {
+    if (errRead) {
+      /* eslint-disable no-console */
+      console.log(errRead);
+      /* eslint-enable */
+      t.fail('Error occur while reading image file');
+      t.end();
+      return;
+    }
+    const base64Image = new Buffer(image, 'binary').toString('base64');
+    const conn = new S3Connector();
+    const opt = {
+      key: 'test-image',
+      body: base64Image
+    };
+
+    conn.putImage(opt, (err, data) => {
+      if (err) {
+        /* eslint-disable no-console */
+        console.log(err);
+        /* eslint-enable */
+        t.fail('Error, putImage fail');
+        t.end();
+        return;
+      }
+      t.ok(data.ETag, 'should have ETag in response');
+      t.end();
+      return;
+    });
+  });
+});
+test('test s3 get image', t => {
+  const conn = new S3Connector();
+  const opt = ['test-image', '11'];
+  const expected = {
+    length: opt.length
+  };
+  conn.getImageUrls(opt, (err, data) => {
+    if (err) {
+      /* eslint-disable no-console */
+      console.log(err);
+      /* eslint-enable */
+      t.fail('Error, getImageUrls');
+      t.end();
+      return;
+    }
+    t.equal(data.length, expected.length, 'should be same length');
+    t.end();
+  });
+});
+test('test s3 delete image', t => {
+  const conn = new S3Connector();
+  const opt = {
+    key: 'test-image'
+  };
+  conn.delImage(opt, (err) => {
+    if (err) {
+      /* eslint-disable no-console */
+      console.log(err);
+      /* eslint-enable */
+      t.fail('Error, delImage');
+      t.end();
+      return;
+    }
+    t.ok(true, 'image removed');
+    t.end();
+  });
+});
