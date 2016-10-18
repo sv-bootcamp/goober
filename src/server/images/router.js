5e1eca7cec3b70c816428b2a4b347a8b84127564
Youngchan Kim <ovekyc@gmail.com>
2016-10-18 15:46:53 +0900

    Edit image module (get)
    Edit get module for not only url but also object
    Edit test code
    Edit MockS3 method
    Edit apidoc

--- src/server/images/router.js
+++ src/server/images/router.js
@@ -4,7 +4,6 @@ import controller from './controllers';
 const router = express.Router();
 
 /**
-<<<<<<< 7dbd01e25568a70efc5cec06094fdb855b47a285
  * @api {get} /images?image=image-unique-id Get an images
  * @apiName get an image
  * @apiGroup Image
@@ -12,28 +11,17 @@ const router = express.Router();
  * @apiParam {String} image ID of target image
  *
  * @apiSuccess {String} image object
-=======
- * @api {get} /images?imageid=image-unique-id Get an images
- * @apiName get an image
- * @apiGroup Image
- *
- * @apiParam {String} imageid ID of target image
- *
- * @apiSuccess {String} imageUrl image access url
->>>>>>> Add image module
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *     {
-<<<<<<< 7dbd01e25568a70efc5cec06094fdb855b47a285
+
  *        key: 'image-8523306706662-c8a94c49-0c3c-414a-bec0-74fc369a105e',
  *        userKey: 'user-1234uuid',
  *        caption: 'thisissmaplecode.',
  *        createdDate: '2016-10-17T08:34:53.338Z',
  *        url: 'url-of-image-8523306706662-c8a94c49-0c3c-414a-bec0-74fc369a105e'
-=======
- *        "imageUrl": "an-images-url"
->>>>>>> Add image module
+
  *     }
  * @apiError (Error 500) Internal Server Error.
  *
