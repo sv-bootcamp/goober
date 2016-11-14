20e1499e94bb4eafe397b52da980953ca8dd0b82
Patrick Shim <nasuk0610@gmail.com>
2016-11-14 15:32:04 +0900

    update apidoc

--- src/server/reports/controllers.js
+++ src/server/reports/controllers.js
@@ -1,4 +1,6 @@
 export default {
+
+  // @TODO you need to implement report method
   post(req, res, cb) {
     res.status(200).send({message: 'success'});
     return cb();
--- src/server/reports/router.js
+++ src/server/reports/router.js
@@ -2,5 +2,31 @@ import express from 'express';
 import Controllers from './controllers';
 
 const router = express.Router();
-router.post('/:id', Controllers.post);
+
+/**
+ * @api {add} /reports Add an item or image.
+ * @apiName reportAnItem
+ * @apiGroup Report
+ *
+ * @apiParamExample {json} Request-Example:
+ *      {
+ *      }
+ *
+ * @apiSuccess {String} message success
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "success"
+ *     }
+ *
+ * @apiError (Error 500) error internal error
+ * @apiErrorExample {json} Internal-Server-Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       error: "error message ..."
+ *     }
+ */
+router.post('/', Controllers.post);
+
 export default router;
